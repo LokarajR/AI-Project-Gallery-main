1c01675349c480b87c7795ef66653f43efa6c8b7
LokarajR <lokaraj10328@gmail.com>
2026-03-05 10:34:51 +0530

    git

--- repo_handler.py
+++ repo_handler.py
@@ -1,4 +1,14 @@
 import os
+import shutil
+import stat
+
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
 
 def clone_repo(url, folder):
+
+    if os.path.exists(folder):
+        shutil.rmtree(folder, onerror=remove_readonly)
+
     os.system(f"git clone {url} {folder}")
